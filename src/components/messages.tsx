3a33864fd009f9b80270bba5b31b346c13971377
Lucas Ribeiro <lucasaraujodesa@hotmail.com>
2024-08-08 20:19:44 -0300

    feat: add CreateMessagesForm and Messages components

---
+++ src/components/messages.tsx
@@ -0,0 +1,31 @@
+import { useParams } from "react-router-dom";
+import { Message } from "./message";
+import { getRoomMessages } from "../http/get-room-messages";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+export function Messages() {
+  const { roomID } = useParams()
+
+  if (!roomID) {
+    throw new Error("Messages component must be used within room page")
+  }
+
+  const { data } = useSuspenseQuery({
+    queryKey: ['messages', roomID],
+    queryFn: () => getRoomMessages({ roomId: roomID }),
+  })
+
+  return (
+    <ol className="list-decimal list-outside px-3 space-y-8">
+      {data.messages.map(message => {
+        return (
+          <Message
+            key={message.id}
+            text={message.text}
+            amountOfReactions={message.amountOfReactions} 
+          />
+        )
+      })}
+    </ol>
+  )
+}
--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,8 +1,10 @@
 import { useParams } from "react-router-dom"
 import amaLogo from '../assets/ama-logo.svg'
-import { ArrowRight, Share2 } from "lucide-react"
+import { Share2 } from "lucide-react"
 import { toast } from "sonner"
-import { Message } from "../components/message"
+import { Messages } from "../components/messages"
+import { Suspense } from "react"
+import { CreateMessageForm } from "../components/create-message-form"
 
 export function Room() {
   const {roomID} = useParams()
@@ -41,31 +43,11 @@ export function Room() {
 
       <div className="h-px 2-full bg-zinc-900" />
 
-      <form className='flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1'>
-          <input
-            type="text"
-            name='theme'
-            placeholder='Qual a sua pergunta'
-            autoComplete='off'
-            className='flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500'
-          />
-          <button
-            type='submit'
-            className='bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm hover:bg-orange-500 transition-colors'
-          >
-            Criar Pergunta
-            <ArrowRight
-              className='size-4'
-            />
-          </button>
-        </form>
+        <CreateMessageForm />
 
-        <ol className="list-decimal list-outside px-3 space-y-8">
-          <Message text="Como funciona as goroutines com o GoLang e por que elas são tão importante para a concorrência e paralelismo" amountOfReactions={10} answered />
-          <Message text="Como funciona as goroutines com o GoLang e por que elas são tão importante para a concorrência e paralelismo" amountOfReactions={10} />
-          <Message text="Como funciona as goroutines com o GoLang e por que elas são tão importante para a concorrência e paralelismo" amountOfReactions={10} />
-          <Message text="Como funciona as goroutines com o GoLang e por que elas são tão importante para a concorrência e paralelismo" amountOfReactions={10} />
-        </ol>
+        <Suspense fallback={<p>Carregando...</p>}>
+          <Messages />
+        </Suspense>
     </div>
   )
 }
