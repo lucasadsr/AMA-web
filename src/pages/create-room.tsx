f5e91ee903c2c9a2f9489a45468282b114586976
Lucas Ribeiro <lucasaraujodesa@hotmail.com>
2024-08-08 20:21:54 -0300

    feat: link CreateRoom with API

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,14 +1,26 @@
 import { ArrowRight } from 'lucide-react'
 import amaLogo from '../assets/ama-logo.svg'
 import { useNavigate } from 'react-router-dom'
+import { createRoom } from '../http/create-room'
+import { toast } from 'sonner'
 
 export function CreateRoom() {
   const navigate = useNavigate()
 
-  function handleCreateRoom(data: FormData) {
+  async function handleCreateRoom(data: FormData) {
     const theme = data.get('theme')?.toString()
 
-    navigate(`room/${theme}`)
+    if (!theme) {
+      return 
+    }
+
+    try {
+      const { roomId } = await createRoom({ theme })
+      navigate(`/room/${roomId}`)
+    } catch (err) {
+      toast.error("Falha ao criar a sala.")
+      console.log(err)
+    }
   }
 
   return (
@@ -26,6 +38,7 @@ export function CreateRoom() {
             placeholder='Nome da sala'
             autoComplete='off'
             className='flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500'
+            required
           />
           <button
             type='submit'
