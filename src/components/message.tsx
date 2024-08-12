0617b6a252e46c150163f2e37bfd466ff2a4850e
Lucas Ribeiro <lucasaraujodesa@hotmail.com>
2024-08-12 19:14:51 -0300

    feat: add message reaction function

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -1,17 +1,60 @@
 import { ArrowUp } from "lucide-react";
 import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { createMessageReaction } from "../http/create-message-reaction";
+import { toast } from "sonner";
+import { removeMessageReaction } from "../http/remove-message-reaction";
 
 interface MessageProps {
+  id: string
   text: string
   amountOfReactions: number
   answered?: boolean
 }
 
-export function Message({ text, amountOfReactions, answered = false }: MessageProps) {
+export function Message({ id: messageId, text, amountOfReactions, answered = false }: MessageProps) {
+  const { roomID } = useParams()
+
   const [hasReacted, setHasReacted] = useState(false)
 
-  function handleReactToMessage() {
+  if (!roomID) {
+    throw new Error("Messages component must be used within room page")
+  }
+
+  async function createMessageReactionAction() {
+    if (!roomID) {
+      return 
+    }
+
+    try {
+      await createMessageReaction({
+        messageId,
+        roomId: roomID
+      })
+    } catch (err) {
+      toast.error("Falha ao curtir mensagem, tente novamente!")
+      console.log(err)
+    }
+
     setHasReacted(true)
+  }
+
+  async function removeMessageReactionAction() {
+    if (!roomID) {
+      return 
+    }
+
+    try {
+      await removeMessageReaction({
+        messageId,
+        roomId: roomID
+      })
+    } catch (err) {
+      toast.error("Falha ao remover curtida da mensagem, tente novamente!")
+      console.log(err)
+    }
+
+    setHasReacted(false)
   }
 
   return (
@@ -19,12 +62,12 @@ export function Message({ text, amountOfReactions, answered = false }: MessagePr
       {text}
 
       {hasReacted ? (
-        <button type="button" className="mt-3 flex items-center gap-2 text-orange-400 text-sm font-medium hover:text-orange-500">
+        <button onClick={removeMessageReactionAction} type="button" className="mt-3 flex items-center gap-2 text-orange-400 text-sm font-medium hover:text-orange-500">
           <ArrowUp className="size-4" />
           Curtir pergunta ({amountOfReactions})
         </button>
       ) : (
-        <button onClick={handleReactToMessage} type="button" className="mt-3 flex items-center gap-2 text-zinc-400 text-sm font-medium hover:text-zinc-300">
+        <button onClick={createMessageReactionAction} type="button" className="mt-3 flex items-center gap-2 text-zinc-400 text-sm font-medium hover:text-zinc-300">
           <ArrowUp className="size-4" />
           Curtir pergunta ({amountOfReactions})
         </button>
